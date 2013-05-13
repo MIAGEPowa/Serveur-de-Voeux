670cffb268dd6d5099c5fee48028e682d7195d3d
vtriquet <virgil.triquet@gmail.com>
2013-05-13 11:57:41 +0200

    Ajout mise à jour des niveaux

---
+++ views/NiveauController/update.php
@@ -0,0 +1,53 @@
+<div id="main">
+
+	<!-- Colonne -->
+	<?php include(LAYOUT_DIR.'sidebar.php'); ?>
+
+	<div id="content">
+		
+		<div id="contentTitle">
+			<h2>Niveaux</h2>
+		</div>
+		
+		<div id="breadcrumb" class="text">
+			<a href="<?php echo WEBROOT.'niveau/index';?>" title="">Niveaux</a><span class="delimiter">></span>Modification d'un niveau
+		</div>
+		
+		<div class="text text-full">
+			<form id="form-update-level" action="<?php echo WEBROOT.'niveau/index';?>" method="post">
+				<fieldset>
+					<legend ><span class="icon-heart"></span>Modifier un niveau</legend>
+					<div>
+          
+            <input type="hidden" id="idLevel" name="idLevel" value="<?php echo $info_level[0]['id'];?>"/>
+            
+						<div class="form-item">
+							<label for="intitule">Intitulé *</label>
+							<input type="text" id="intitule" name="intitule" value="<?php echo $info_level[0]['libelle'];?>" class="input-large" />
+						</div>
+						
+						<div class="form-item">
+							<label for="description">Diplôme *</label>
+							<select id="diplome" name="diplome">
+                <?php foreach ($degrees as $d) { ?>
+                  <?php if ($d['id'] == $info_level[0]['id_diplome']) { ?>
+                    <option selected="selected" value="<?php echo $d['id'];?>"><?php echo $d['libelle'];?></option>
+                  <?php } else { ?>
+                    <option value="<?php echo $d['id'];?>"><?php echo $d['libelle'];?></option>
+                <?php 
+                    }
+                  }; 
+                ?>
+              </select>
+						</div>
+						<div class="form-item">
+							<input type="submit" class="input-submit input-submit-orange" value="Modifier" />
+						</div>
+						
+					</div>
+				</fieldset>
+			</form>
+		</div>
+		
+	</div>
+</div>
--- views/js/tools.js
+++ views/js/tools.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
 	
+  //
+	// Déroule le formulaire d'ajout
+	//
+	$(".button-slide").click(function() {
+		if($(this).next().css('display') == 'none') {
+			$(this).next().slideDown('medium');
+			$(this).find(".icon-arrow").animate({
+				myRotationProperty: -180
+			},
+			{
+				step: function(now, tween) {
+					if (tween.prop === "myRotationProperty") {
+						$(this).css('-webkit-transform','rotate('+now+'deg)');
+						$(this).css('-moz-transform','rotate('+now+'deg)'); 
+						// add Opera, MS etc. variants
+						$(this).css('transform','rotate('+now+'deg)');  
+					}
+				}
+			});
+		}
+		else {
+			$(this).next().slideUp('medium');
+			$(this).find(".icon-arrow").animate({
+				myRotationProperty: 0
+			},
+			{
+				step: function(now, tween) {
+					if (tween.prop === "myRotationProperty") {
+						$(this).css('-webkit-transform','rotate('+now+'deg)');
+						$(this).css('-moz-transform','rotate('+now+'deg)'); 
+						// add Opera, MS etc. variants
+						$(this).css('transform','rotate('+now+'deg)');  
+					}
+				}
+			});
+		}
+	});
+  
 	// Ferme le overlay quand l'utilisateur clique sur la croix en haut a droite du bloc
 	$(".overlayClose").click(function() {
 		var div = $(this).parent().parent();
