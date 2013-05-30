385b266afc8b28a103bd3ff59fc997515190ee77
kbrivois <kevbriv@hotmail.fr>
2013-05-30 02:10:11 +0200

    [*] centrage des pourcentages dans les barres de progression

--- views/js/configuration.js
+++ views/js/configuration.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-	
-	//
-	// Lance un fenêtre de confirmation quand l'utilisateur souhaite passer à l'année suivante
-	//
-	
-	$('#form-config').submit(function() {
-        if (confirm('Voulez-vous vraiment passer à l\'année suivante ?')) {
-            return true;
-        }
-		return false;
-	});
-	
+$(document).ready(function() {
+	
+	//
+	// Lance un fenêtre de confirmation quand l'utilisateur souhaite passer à l'année suivante
+	//
+	
+	$('#form-config').submit(function() {
+        if (confirm('Voulez-vous vraiment passer à l\'année suivante ?')) {
+            return true;
+        }
+		return false;
+	});
+	
 });
