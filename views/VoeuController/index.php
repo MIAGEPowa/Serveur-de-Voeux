e9fcad5eaf942fa9c6089c0f3d84cba8cdc42796
AAymeric <aymeric.auberton@hotmail.fr>
2013-05-31 03:15:17 +0200

    [+] Ajouter/Modifier un voeu depuis FiliereEnseignement/view [*] Amélioration de la page mes voeux

--- views/VoeuController/index.php
+++ views/VoeuController/index.php
@@ -6,72 +6,85 @@
 	<div id="content">
 	
 		<div id="contentTitle">
-			<h2>V&oelig;ux</h2>
+			<h2>Mes v&oelig;ux</h2>
 		</div>
 		
 		<div id="breadcrumb" class="text">
-			<a href="<?php echo WEBROOT.'voeu/index'; ?>" title="">V&oelig;ux</a>
+			<a href="<?php echo WEBROOT.'voeu/index'; ?>" title="">Mes v&oelig;ux</a>
 		</div>
 		
-		<div class="text text-two">
-      <?php 
-      $f_id = ''; // id de la filière
-      $f_libelle = ''; // libelle de la filiere = niveau - specialite - apprentissage
-      $f_num = 0; // numéro du 'bloc' afin de placer à gauche ou à droite ce dernier
-      $f_bool = false; // test afin de déterminer si on est toujours dans le même bloc
-      $apprentissage = '';
-      
-      foreach ($filiereEnseignement as $f) { 
-        
-        if (($f_id == '') || ($f_id != $f['id_filiere'])) {
-          $f_id = $f['id_filiere'];
-          if ($f['apprentissage'] == 1) $apprentissage = 'Apprentissage'; else $apprentissage = 'Initial';
-          $f_libelle = $f['libelle_niveau'].' '.$f['libelle_specialite'].' '.$apprentissage;
-          $f_bool = true;
-          $f_num = $f_num + 1;
-        }
-        
-        if ($f_bool == true) {
-          if ($f_num > 1) {
-            echo '</tbody>';
-            echo '</table>';
-            echo '</div>';
-          }
-          if ($f_num % 2 != 0)
-            echo '<div class="text-two-item text-two-item-first" style="clear: both;">';
-          else
-            echo '<div class="text-two-item">';
-          /*echo '<div class="text-two-item">';*/
-          echo '<h2>'.$f_libelle.'</h2>';
-          echo '<table>
-                  <thead>
-                    <tr>
-                      <th width="30%">Enseignement</th>
-                      <th width="20%">Année</th>
-                      <th width="20%">Cours</th>
-                      <th width="20%">TD</th>
-                      <th>Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>';    
-        }
-          
-          echo '<tr>';
-            echo '<td>'.$f['libelle_enseignement'].'</td>';
-            echo '<td>'.$f['annee'].'</td>';
-            echo '<td>'.$f['nbr_h_cours'].'</td>';
-            echo '<td>'.$f['nbr_h_td'].'</td>';
-            echo '<td>';
-              echo '<a class="buttons-link" href='.WEBROOT.'voeu/update/'.$f['id_filiere_enseignement'].'><span class="buttons button-orange">Modifier</span></a>';
-              echo '<a class="buttons-link" href='.WEBROOT.'voeu/delete/'.$f['id_filiere_enseignement'].'><span class="buttons button-red">Supprimer</span></a>';
-            echo '</td>';
-          echo '</tr>';
-          
-        if ($f_bool == true)
-          $f_bool = false;
-      } ?>
+		<div class="text text-full">
+			<h2>Liste de vos v&oelig;ux</h2>
+			
+			<table>
+				 <thead>
+					<tr>
+						<th width="18%">Filière</th>
+						<th width="18%">Enseignement</th>
+						<th width="8%" align="center">Année</th>
+						<th width="10%" align="center">Nb h cours</th>
+						<th width="10%" align="center">Nb h TD</th>
+						<th width="10%" align="center">Nb h TP</th>
+						<th width="11%">Conflits</th>
+						<th width="15%">Actions</th> <!-- Visualiser et Supprimer -->
+					</tr>
+				</thead>
+				<tbody>
+				
+					<?php 
+						foreach($fee as $f) { 
+							$voeur_utilisateur_h_cours = round($f['nbr_h_cours'] / 60, 2);
+							$voeur_utilisateur_h_td = round($f['nbr_h_td'] / 60, 2);
+							$voeur_utilisateur_h_tp = round($f['nbr_h_tp'] / 60, 2);
+					?>
+						
+						<tr>
+							<td><?php echo $f['filiere']; ?></td>
+							<td><?php echo $f['libelle']; ?></td>
+							<td align="center"><?php echo $f['annee']; ?></td>
+							<td align="center"><?php echo $voeur_utilisateur_h_cours; ?> h</td>
+							<td align="center"><?php echo $voeur_utilisateur_h_td; ?> h</td>
+							<td align="center"><?php echo $voeur_utilisateur_h_tp; ?> h</td>
+							<td>
+								<?php
+									if($f['conflits']) {
+										// Cours
+										if(isset($f['conflits']['cours']) && isset($f['conflits']['cours_conflit'])) {
+											if(!$f['conflits']['cours_conflit'])
+												echo 'Cours (<strong><span class="orange">- '.round($f['conflits']['cours'] / 60, 2).'</span></strong>)<br />';
+											else
+												echo 'Cours (<strong><span class="red">+ '.round($f['conflits']['cours'] / 60, 2).'</span></strong>)<br />';
+										}
+										
+										// TD
+										if(isset($f['conflits']['td']) && isset($f['conflits']['td_conflit'])) {
+											if(!$f['conflits']['td_conflit'])
+												echo 'TD (<strong><span class="orange">- '.round($f['conflits']['td'] / 60, 2).'</span></strong>)<br />';
+											else
+												echo 'TD (<strong><span class="red">+ '.round($f['conflits']['td'] / 60, 2).'</span></strong>)<br />';
+										}
+										
+										// TP
+										if(isset($f['conflits']['tp']) && isset($f['conflits']['tp_conflit'])) {
+											if(!$f['conflits']['tp_conflit'])
+												echo 'TP (<strong><span class="orange">- '.round($f['conflits']['tp'] / 60, 2).'</span></strong>)<br />';
+											else
+												echo 'TP (<strong><span class="red">+ '.round($f['conflits']['tp'] / 60, 2).'</span></strong>)<br />';
+										}
+									}
+								?>
+							</td>
+							<td>
+								<a class="buttons-link" href="<?php echo WEBROOT; ?>filiereEnseignement/view/<?php echo $f['id_filiere_enseignement']; ?>"><span class="buttons button-green">Visualiser</span></a>
+								<a class="buttons-link" href="<?php echo WEBROOT; ?>"><span class="buttons button-red">Supprimer</span></a>
+							</td>
+						</tr>
+						
+					<?php } ?>
+				
+				</tbody>
+			</table>
 		</div>
 
-		
 	</div>
 </div>
